f9bb5f2f7b291865a5f0b87b2fdb9bfef6b3d6ba
Elie-Tis <elie.tisseur@cohb-industrie.com>
2025-02-06 15:36:17 +0100

    Update voiles.py

--- voiles.py
+++ voiles.py
@@ -392,7 +392,7 @@ def verifier_torseurs_voiles_etages(df_torseurs_voiles_rupt, df_torseurs_voiles_
     df_torseurs_voiles_glob.rename(columns={"Cas_de_charges_rupt": "Cas_de_charges",
                                             "Nom_Etage_rupt": "Etage"}, inplace=True)
 # Modification de l'ordre des colonnes
-    df_torseurs_voiles_glob = df_torseurs_voiles_glob[["Etage","Cas_de_charges", "TX_base", "TY_base", "TX_rupt", "TY_rupt", "Ecart_TX", "Ecart_TY"]]
+    df_torseurs_voiles_glob = df_torseurs_voiles_glob.loc[:,["Etage","Cas_de_charges", "TX_base", "TY_base", "TX_rupt", "TY_rupt", "Ecart_TX", "Ecart_TY"]]
     df_torseurs_voiles_glob.sort_values(by=["Etage","Cas_de_charges"], ascending=[False, True], inplace=True)
     df_torseurs_voiles_defect = df_torseurs_voiles_glob.loc[df_torseurs_voiles_glob["Ecart"] >= ecart_limite, :]
     if df_torseurs_voiles_defect.empty:
