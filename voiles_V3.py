2aba0c2868ef225d0f327513a965b1106c07dabf
Elie-Tis <elie.tisseur@cohb-industrie.com>
2025-02-12 16:10:31 +0100

    Create voiles_V3.py

---
+++ voiles_V3.py
@@ -0,0 +1,3 @@
+# Lorsque les voiles ne sont pas numérotés de la même manière entre le modèle avec et sans rupteurs, la comparaison des efforts dans les voiles de manière individuelle ne peux plus se baser sur les numéros d'éléments.
+# Il faut donc se rabattre sur l"utilisation des coordonées des voiles. Important de noter que cette méthode ne fonctionne pas dans le cas de modèles avec des refends désolidarisés de la façade !!!!
+
