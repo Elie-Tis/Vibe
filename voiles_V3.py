bd628ac07494a43e0346731260c79e2c94f68372
Elie-Tis <elie.tisseur@cohb-industrie.com>
2025-02-14 08:47:56 +0100

    Update voiles_V3.py

--- voiles_V3.py
+++ voiles_V3.py
@@ -169,6 +169,7 @@ def get_efforts_voiles(page_coord_voiles, page_epaisseurs_voiles, page_efforts_v
 def analyse_efforts_voiles(df_efforts_voiles_rupt, df_efforts_voiles_base,list_effort=["Txy_bas", "Txy_haut"], dict_cdc_dir=dict("3 (CQC)"="x", "4 (CQC)"="y" ):
 	df_ecarts_efforts_voiles = calc_ecart_efforts_voiles(df_efforts_voiles_rupt, df_efforts_voiles_base,list_effort)
 	df_ecart_moy_pond = df_cal_moy_pond_ecarts_voiles(df_ecarts_efforts_voiles, dict_cdc_dir)
+
 	return df_ecart_moy_pond
 	
 	
