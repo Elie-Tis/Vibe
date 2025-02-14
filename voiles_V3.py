8d7f7f36703dc6122f8c3f61dfdf04a69fdc24be
Elie-Tis <elie.tisseur@cohb-industrie.com>
2025-02-14 13:33:57 +0100

    f

--- voiles_V3.py
+++ voiles_V3.py
@@ -161,7 +161,7 @@ def calc_moy_pond_ecarts_voiles(df_ecart_efforts_voiles, dict_cdc_dir):
         for col_ecart in [col for col in  df_ecart_efforts_voiles.columns if "ecart" in col]:  # Pondération de chaque colonne écart
             df_ecart_efforts_voiles.loc[filtre_cdc, [f"{col_ecart}_pond"]] = df_ecart_efforts_voiles.loc[filtre_cdc, [col_ecart]] * df_ecart_efforts_voiles.loc[filtre_cdc,[Idir]] /  sum_I
         
-    return df_ecart_moy_pond
+    return df_ecart_efforts_voiles
 
 
 
