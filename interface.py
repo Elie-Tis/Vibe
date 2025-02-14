1b00a628120f662a04b31a1f566bd5cfb5856f2d
Elie-Tis <elie.tisseur@cohb-industrie.com>
2025-02-14 11:02:59 +0100

    v

--- interface.py
+++ interface.py
@@ -340,4 +340,5 @@ df_efforts_voiles_base = voiles_V3.get_efforts_voiles(page_coord_voiles=pages_ba
                     list_cdc=["3 (CQC)", "4 (CQC)"])
 
 st.dataframe(df_efforts_voiles_rupt)
+st.dataframe(df_efforts_voiles_base)
 voiles_V3.analyse_efforts_voiles(df_efforts_voiles_rupt, df_efforts_voiles_base,list_effort=["Txy_bas", "Txy_haut"], dict_cdc_dir={"3 (CQC)":"x", "4 (CQC)":"y"})
