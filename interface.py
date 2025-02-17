ebf712b124a3b66b70fedb050f162121ecce1e3b
Elie-Tis <elie.tisseur@cohb-industrie.com>
2025-02-17 11:57:09 +0100

    f

--- interface.py
+++ interface.py
@@ -85,7 +85,7 @@ else:
     ndc_ex = open("BAT_A_SLABE_ndc08.txt", "r", encoding="utf-16")
     ndc_rupteur = ndc_ex.read()
     ndc_ex.close()
-    ndc_ex = open("BAT_A_BETON_ndc14.txt", "r", encoding="utf-16")
+    ndc_ex = open("BAT_A_BETON_ndc15.txt", "r", encoding="utf-16")
     ndc_base = ndc_ex.read()
     ndc_ex.close()
 # Découpage des ndc en différentes pages
